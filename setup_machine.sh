4a326b210fdc35f54a5c036a67c07dc4c4be9fec
Avery Lamp <averylamp@gmail.com>
2020-05-26 01:48:24 -0400

    Adds dotfiles

---
+++ setup_machine.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+
+echo Installing apt packages
+sudo apt-get install parallel vim
+
+echo Setup Zsh
+sudo apt-get install zsh curl wget
+
+echo copying dotfiles
+cp dotfiles/* ~/
+
+echo install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+
