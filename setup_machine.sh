69aaaa0102f9126d6e170d726bfb2e90ce4dd65d
Avery Lamp <averylamp@gmail.com>
2020-05-26 02:15:00 -0400

    Adds server network configs

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 echo Installing apt packages
-sudo apt-get install parallel vim
+sudo apt-get install parallel vim git
 
 echo Setup Zsh
 sudo apt-get install zsh curl wget
@@ -11,6 +11,8 @@ cp dotfiles/* ~/
 
 echo install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo chsh -s $(which zsh)
 
 
 
